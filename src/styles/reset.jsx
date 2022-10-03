a4e46cd2367a696d1db29020bcfcc2f0c2019453
NattanSIlva <natanxboy@gmail.com>
2022-10-03 15:50:11 -0300

    Add: core pages structure

--- src/styles/reset.jsx
+++ src/styles/reset.jsx
@@ -1,12 +1,30 @@
 const { createGlobalStyle } = require("styled-components");
 
 export const GlobalStyle = createGlobalStyle`
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  border: none;
-  list-style: none;
-  text-decoration: none;
+  :root {
+    --primary: #FF577F;
+    --primary-50: #FF427F;
+    --primary-disable: #59323F;
+
+    --gray-4: #121214;
+    --gray-3: #212529;
+    --gray-2: #343B41;
+    --gray-1: #868E96;
+
+    --feedback-sucess: #3FE864;
+    --feedback-error: #E83F5B;
+  }
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+    list-style: none;
+    text-decoration: none;
+    font-family: 'Inter', sans-serif;
+
+  }
 
   button {
     display: flex;
